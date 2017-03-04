87397ae08d656ec6e0b221360947dc2fa5944de8
mightyalex200 <quantumtraveling@gmail.com>
2017-03-04 12:35:07 -0500

    added ammo counter under gun in gunbelt

--- js/gunbelt.js
+++ js/gunbelt.js
@@ -4,6 +4,7 @@ class Gunbelt extends PIXI.Container{
     this.guns = [];
     this.sprites = [];
     this.labels = [];
+    this.ammolabels = [];
   }
   update(){
     // 49-57
@@ -28,8 +29,15 @@ class Gunbelt extends PIXI.Container{
         this.labels[gunn].visible = false;
         app.stage.addChild(this.labels[gunn]);
       }
+      if(!this.ammolabels[gunn]){
+        this.ammolabels[gunn] = new PIXI.Text();
+        this.ammolabels[gunn].position.set(gunn*100+32,512-this.ammolabels[gunn].height);
+        app.stage.addChild(this.ammolabels[gunn]);
+      }
       if(this.guns[gunn] !== undefined){
         this.labels[gunn].visible = true;
+        this.ammolabels[gunn].visible = true;
+        this.ammolabels[gunn].text = this.guns[gunn].ammo;
         if (!this.sprites[gunn]) {
           this.sprites[gunn] = new PIXI.Sprite(this.guns[gunn].texture);
           this.sprites[gunn].position.set(gunn*100,448);
@@ -39,6 +47,7 @@ class Gunbelt extends PIXI.Container{
         }
       }else{
         this.labels[gunn].visible = false;
+        this.ammolabels[gunn].visible = false;
         app.stage.removeChild(this.sprites[gunn]);
         this.sprites.splice(gunn,1);
       }
