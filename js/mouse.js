46c48a9bff9810d82003d2c26f5e99f595c68861
MightyAlex200 <quantumtraveling@gmail.com>
2017-03-06 12:14:32 -0500

    fixed mouse movement on displays other than 1080p

--- js/mouse.js
+++ js/mouse.js
@@ -4,8 +4,8 @@ document.mouse = {
   isDown: false
 };
 document.onmousemove = (e) => {
-  document.mouse.x = ((e.clientX+1)/innerWidth)*screen.width;
-  document.mouse.y = ((e.clientY+1)/innerHeight)*screen.height;
+  document.mouse.x = ((e.clientX+1)/innerWidth)*app.stage.width;
+  document.mouse.y = ((e.clientY+1)/innerHeight)*app.stage.height;
 };
 
 document.onmousedown = (e) => {
