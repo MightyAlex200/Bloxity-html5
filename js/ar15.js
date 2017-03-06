278ec78b714f45baf239a01fd61fe1b35a98c13e
mightyalex200 <quantumtraveling@gmail.com>
2017-03-05 21:13:41 -0500

    fixed ar15 clip size

--- js/ar15.js
+++ js/ar15.js
@@ -4,6 +4,7 @@ class AR15 extends Gun {
     this.cooldown = 5;
     this.ammo = 500;
     this.clip = 32;
+    this.clipsize = 32;
     this.ammomultiplier = 500;
     this.texture = PIXI.loader.resources["res/img/ar15.png"].texture;
   }
